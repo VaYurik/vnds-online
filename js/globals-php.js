97047d3272fce83404d5f66eb5fe8484c9a8d7d6
VaYurik <31892828+VaYurik@users.noreply.github.com>
2018-04-02 13:10:45 +0000

    Delete globals-php.js

--- js/globals-php.js
+++
@@ -1,8 +0,0 @@
-let G = {
-	"post-log": true,
-	"log-url": "php/save_log.php",
-	"show-promo": true,
-	"promo-url": "php/get_promo.php",
-	"promo-redirect": "php/redirect_promo.php",
-	"games-list": "php/get_games_list.php"
-};
